34977ad30d1157ea7d6c68ef8bb09fe8a60f3482
BesedaDi <dianabesedina223@gmail.com>
2020-09-15 18:28:15 +0700

    continue

--- storage.py
+++ storage.py
@@ -3,36 +3,57 @@ import tempfile
 import json
 import argparse
 
+
 def pars_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--key', dest='key_name')
     parser.add_argument('--val', dest='value')
     return parser.parse_args()
 
-def read_storage(file_name):
-    with open(file_name) as f:
-        storage = json.load(f)
 
+def read_storage():
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    with open(storage_path, 'r') as f:
+        storage = json.load(f)
     return storage
+
 
 def rec_storage(storage):
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
     with open(storage_path, 'w') as f:
         json.dump(storage, f)
 
-def work():
-    args = pars_arg()
-    storage = dict()
-    key = args.key_name
-    val = args.value
-    if key not in storage:
-        storage[key] = val
-    else:
-        
 
+args = pars_arg()
+storage = read_storage()
+key = args.key_name
+val = args.value
+if key != None and val != None:
+    if key not in storage:
+        storage[key] = list(str(val))
+    else:
+        for k, v in storage.items():
+            if key == k:
+                if isinstance(v, list):
+                    storage[key].append(v)
+                else:
+                    storage[key] = list(val)
+                    storage[key].append(v)
+
+    print(storage)
     print('Key "{}" = {}'.format(key, val))
+
+    # print(storage)
+
     rec_storage(storage)
 
+elif key != None:
+    storage = read_storage()
 
+    if key in storage.keys():
+        for k, v in storage.items():
+            if k == key:
+                print(v)
 
-
+    else:
+        print(None)
--- test.py
+++ test.py
@@ -1,1 +1,76 @@
-print()
+import os
+import tempfile
+import json
+import os
+import tempfile
+import json
+import argparse
+
+
+def pars_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key', dest='key_name')
+    parser.add_argument('--val', dest='value')
+    return parser.parse_args()
+
+
+def read_storage():
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    with open(storage_path, 'r') as f:
+        storage = json.load(f)
+    return storage
+
+
+def rec_storage(storage):
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    with open(storage_path, 'w') as f:
+        json.dump(storage, f)
+
+
+args = pars_arg()
+storage = dict()
+key = args.key_name
+val = args.value
+storage_file_name = os.path.join(os.path.dirname(__file__), 'storage.data')
+if key != None and val != None:
+    if not os.path.exists(storage_file_name):
+        lst = []
+        if key not in storage:
+            storage[key] = val
+        else:
+            for k, v in storage.items():
+                if key == k:
+                    lst.append(val)
+                    storage[key] = lst
+                    storage[key] = lst.append(v)
+        print('Key "{}" = {}'.format(key, val))
+        print(storage)
+        print('123456789')
+
+        rec_storage(storage)
+    else:
+        storage = read_storage()
+        lst = []
+        if key not in storage:
+            storage[key] = val
+        else:
+            for k, v in storage.items():
+                if key == k:
+                    lst.append(val)
+                    storage[key] = lst
+                    storage[key] = lst.append(v)
+        print('Key "{}" = {}'.format(key, val))
+        rec_storage(storage)
+    print(storage)
+elif key != None:
+    storage = read_storage()
+    print(storage)
+
+    el = ''
+    for k, v in storage.items():
+        print(k, v)
+        if k == key:
+            el = v
+            print(v)
+    if el == '':
+        print(None)
